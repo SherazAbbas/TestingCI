7152b722112cf40990e94b04b8ed100c7b566bce
Sheraz Abbas <sherazshahbukhari12@gmail.com>
2024-04-03 14:13:38 +0500

    Pass case

--- cypress/e2e/testAssertion.cy.js
+++ cypress/e2e/testAssertion.cy.js
@@ -2,6 +2,6 @@ describe('Assertion', () => {
     it('Search assertion', () => {
       cy.visit('https://www.google.com/')
       cy.get('#APjFqb').focus().type('cypress{Enter}')
-      cy.get('#APjFqb').should('have.value','cypress1')
+      cy.get('#APjFqb').should('have.value','cypress')
     })
   })
